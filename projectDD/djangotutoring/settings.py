2a1c658e7fa9eb94c4dd2bc6a369a30fccadf5bf
[Maxim Tiberneac] <[mtiberneac@gmail.com]>
2023-06-23 12:54:24 -0400

    Help, me no saber quoi faire. Brach to delete soon

--- projectDD/djangotutoring/settings.py
+++ projectDD/djangotutoring/settings.py
@@ -119,13 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'projectDD')
-STATIC_URL = '/src/'
-ADMIN_MEDIA_PREFIX = '/src/admin/'
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'src'),
 ]
